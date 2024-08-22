86e51fb24d6f0d1af1449b4088b5854534596ba5
josueramirez1 <josue67ramirez@gmail.com>
2024-08-22 12:41:32 -0700

    fixing grid layouts order to have each tabbed browser have the same grid layout
    provides a consistent tabbed browsing experience

--- src/hero.js
+++ src/hero.js
@@ -8,17 +8,20 @@ export default function home() {
   //   hero section
   const hero = Section().section;
   hero.setAttribute("id", "section-hero");
-  hero.classList.add("grid-2-columns-uneven");
   content.appendChild(hero);
 
   // 1st column
   const container = Container().container;
   container.classList.add("container");
   container.classList.add("hero");
+  container.classList.add("grid-2-columns");
 
   const restaurantName = document.createElement("h1");
   restaurantName.classList.add("restaurant-title");
   restaurantName.textContent = "dine";
+
+  const heroColumn1 = document.createElement("div");
+  heroColumn1.classList.add("hero-column-1");
 
   const heroBox = document.createElement("div");
   heroBox.classList.add("hero-box");
@@ -33,13 +36,14 @@ export default function home() {
     "Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.";
 
   hero.appendChild(container);
-  container.appendChild(heroBox);
+  container.appendChild(heroColumn1);
+  heroColumn1.appendChild(heroBox);
   heroBox.appendChild(restaurantName);
   heroBox.appendChild(heroHeading);
   heroBox.appendChild(paragraphHeading);
 
   // 2nd column
-  const heroImage = document.createElement("div");
-  heroImage.classList.add("hero-img-column");
-  hero.appendChild(heroImage);
+  const heroColumn2 = document.createElement("div");
+  heroColumn2.classList.add("hero-column-2");
+  container.appendChild(heroColumn2);
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import menu from "./menu.js";
 import contactUs from "./contact.js";
 
 // const start = hero();
-// const foodMenu = menu();
-const contactPage = contactUs();
+const foodMenu = menu();
+// const contactPage = contactUs();
