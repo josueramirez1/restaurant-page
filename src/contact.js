2437b73425ee84bf91afebf75ed1ab901e661d8c
josueramirez1 <josue67ramirez@gmail.com>
2024-08-23 22:10:25 -0700

    adding tab functionality in order to have website be able to switch pages when clicking button
    allows users to click on tabs to browse website without scrolling

--- src/contact.js
+++ src/contact.js
@@ -16,4 +16,29 @@ export default function contactUs() {
   restaurantName.textContent = "dine";
 
   container.appendChild(restaurantName);
+
+  const city = document.createElement("p");
+  city.classList.add("city");
+  city.innerHTML = "Marthwaite, Sedbergh <br/> Cumbria";
+
+  const phoneNumber = document.createElement("p");
+  phoneNumber.classList.add("phone-number");
+  phoneNumber.textContent = "+00 44 123 4567";
+
+  const address = document.createElement("div");
+  address.classList.add("address");
+  address.appendChild(city);
+  address.appendChild(phoneNumber);
+
+  container.appendChild(address);
+
+  const hoursOfOperation = document.createElement("div");
+  hoursOfOperation.classList.add("hours-of-operation");
+  container.appendChild(hoursOfOperation);
+
+  const hours = document.createElement("p");
+  hours.innerHTML =
+    "Open Hours: <br/> Mon - Fri: 09:00 AM - 10:30 PM <br/> Sat - Sun: 09:00 AM -  11:30 PM ";
+
+  hoursOfOperation.appendChild(hours);
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import "./styles.css";
-import hero from "./hero.js";
 import menu from "./menu.js";
 import contactUs from "./contact.js";
+import home from "./hero.js";
 
-// const start = hero();
-const foodMenu = menu();
-// const contactPage = contactUs();
+function start() {
+  home();
+
+  document.addEventListener("click", (e) => {
+    const content = document.querySelector("#content");
+    const homeBtn = document.querySelector(".nav-btn-1");
+    const menuBtn = document.querySelector(".nav-btn-2");
+    const contactBtn = document.querySelector(".nav-btn-3");
+
+    const section = document.querySelector("section");
+    if (
+      e.target === homeBtn ||
+      e.target === menuBtn ||
+      e.target === contactBtn
+    ) {
+      content.removeChild(section);
+      if (e.target === homeBtn) home();
+      if (e.target === menuBtn) menu();
+      if (e.target === contactBtn) contactUs();
+    }
+  });
+}
+
+const startTab = start();
